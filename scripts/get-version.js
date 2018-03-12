6588e624a73c3349534b91df778511cf4dc87400
Jeremy Brown <jeremy@notmessenger.com>
2018-03-12 11:51:20 -0500

    Closes ciena-blueplanet/ciena-devops#10

--- scripts/get-version.js
+++ scripts/get-version.js
@@ -6,25 +6,41 @@ const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
 const whichPackage = process.argv[2]
-const whichVersion = process.argv[3]
+const packageVersion = process.argv[3]
+const whichVersion = process.argv[4]
 
 /**
  * Get either the current or previous version of an NPM package
  *
  * @param {String} whichPackage NPM package to interrogate
+ * @param {String} packageVersion "version" property value from package.json file
  * @param {?String} whichVersion If 'previous' then previous version, otherwise current
  *
  * @returns {String|undefined} Current or previous version of the NPM package
  */
-async function getVersion(whichPackage, whichVersion) {
+async function getVersion(whichPackage, packageVersion, whichVersion) {
     const {stdout, stderr} = await exec(`npm show ${whichPackage} versions --json`)
 
     if (!stderr) {
-        const offset = (whichVersion === 'previous') ? 1 : 0
         const versions = JSON.parse(stdout)
+        const publishedNpmVersion = versions[versions.length-1]
 
-        console.log(versions[versions.length - 1 - offset])
+        let latest
+        let previous
+
+        // NPM registry has latest package version
+        if (publishedNpmVersion == packageVersion) {
+            latest = publishedNpmVersion
+            previous = versions[versions.length-2]
+
+        // NPM registry does not have latest package version
+        } else {
+            latest = packageVersion
+            previous = versions[versions.length-1]
+        }
+
+        console.log(whichVersion === 'previous' ? previous : latest)
     }
 }
 
-getVersion(whichPackage, whichVersion)
+getVersion(whichPackage, packageVersion, whichVersion)
